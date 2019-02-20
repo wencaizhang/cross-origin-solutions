44a85fb4cb410645ba3ce86bd16f6629cb4f9dcf
wencaizhang <1052642137@qq.com>
2019-02-20 21:09:06 +0800

    gulp-connect 指定本机IP

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -7,10 +7,27 @@ const target = 'http://api.meijujingxuan.com';
 const root = './src';
 const port = 8888;
 
+///////////////////获取本机ip///////////////////////
+const os = require('os');
+function getIPAdress() {
+    var interfaces = os.networkInterfaces();
+    for (var devName in interfaces) {
+        var iface = interfaces[devName];
+        for (var i = 0; i < iface.length; i++) {
+            var alias = iface[i];
+            if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+                return alias.address;
+            }
+        }
+    }
+}
+const myHost = getIPAdress();
+
 gulp.task('server', function () {
   connect.server({
     root: root,
     port: port,
+    host: myHost,
     livereload: true,
     middleware: function (connect, opt) {
       return [
